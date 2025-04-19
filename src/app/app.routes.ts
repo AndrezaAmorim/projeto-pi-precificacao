f43f35c734258e48aa97d86cebcf5b417da5de21
Andreza Amorim <amrimandreza@gmail.com>
2025-04-19 18:36:02 -0300

    Mudanca configuracoes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '**', redirectTo: 'home' },
 ];
